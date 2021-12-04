3d133c43c90019fe7b02f9d173c1ed271b2a93f9
Sam6543210 <sam.ple6543210@gmail.com>
2021-12-04 08:12:00 +0100

    Prathvis's edits

---
+++ HealthKit/Product.swift
@@ -0,0 +1,20 @@
+//
+//  Product.swift
+//  HealthKit
+//
+//  Created by admin on 12/3/21.
+//
+
+import Foundation
+class Product
+{
+    var productName:String
+    var productImage:String
+    var price:String
+    init(pName:String,pImage:String,pPrice:String)
+    {
+        productName = pName
+        productImage = pImage
+        price = pPrice
+    }
+}
--- HealthKit/ViewController.swift
+++ HealthKit/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var productList = [Product]()
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        fillData()
+    }
+    func fillData()
+    {
+        let product1 = Product(pName:"Cream Fantasy (30 g)",pImage:"image1",pPrice:"Rs. 40")
+        productList.append(product1)
+        
+        let product2 = Product(pName:"Cream Fantasy(50g)",pImage:"image1",pPrice:"Rs. 60")
+        productList.append(product2)
     }
     
 }
