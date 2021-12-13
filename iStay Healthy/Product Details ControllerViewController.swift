d3477ab0d0d9bc1e46fe8098ac923696b2867fca
Sam6543210 <sam.ple6543210@gmail.com>
2021-12-13 23:01:03 +0100

    renamed project

---
+++ iStay Healthy/Product Details ControllerViewController.swift
@@ -0,0 +1,22 @@
+//
+//  Product Details ControllerViewController.swift
+//  HealthKit
+//
+//  Created by admin on 12/6/21.
+//
+
+import UIKit
+import HealthKit
+
+let healthKitStore: HKHealthStore = HKHealthStore()
+
+class Product_Details_ControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
