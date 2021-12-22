38387c9c5899c2014c9b73765c173a1a326cddc1
Sam6543210 <sam.ple6543210@gmail.com>
2021-12-22 20:43:13 +0100

    Sams edit

--- iStay Healthy/Product Details ControllerViewController.swift
+++ iStay Healthy/Product Details ControllerViewController.swift
@@ -13,9 +13,6 @@ let healthKitStore: HKHealthStore = HKHealthStore()
 class Product_Details_ControllerViewController: UIViewController {
 
     @IBOutlet weak var passedProductImge: UIImageView!
-    @IBOutlet weak var weightLabel: UILabel!
-    @IBOutlet weak var heightLabel: UILabel!
-    @IBOutlet weak var sodiumLabel: UILabel!
     
     var selectedProduct: Product? = nil
     let healthDetails = HealthDetails_ViewController.init()
@@ -23,7 +20,6 @@ class Product_Details_ControllerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpProductDetail()
-        displayHealthData()
         
         // Do any additional setup after loading the view.
     }
@@ -33,55 +29,6 @@ class Product_Details_ControllerViewController: UIViewController {
         
     }
     
-    
-    func displayHealthData(){
-        
-        guard let weightSampleType = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass) else{
-        print("Weight sample is no longer available in healthkit")
-        return
-        }
-        guard let heightSampleType = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.height) else{
-        print("Height sample is no longer available in healthkit")
-        return
-        }
-        guard let sodiumSampleType = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.dietarySodium) else{
-        print("Sodium sample is no longer available in healthkit")
-        return
-        }
-        healthDetails.readSampleHealthData(for: weightSampleType){
-            (sample, error) in
-            guard let sample = sample else {
-                if let error = error {
-                    self.dislayAlert(for: error)
-                }
-                return
-            }
-            self.weightLabel.text = String(sample.quantity.doubleValue(for: HKUnit(from: "kg")))
-            self.weightLabel.text?.append(" kg")
-        }
-        healthDetails.readSampleHealthData(for: heightSampleType){
-            (sample, error) in
-            guard let sample = sample else {
-                if let error = error {
-                    self.dislayAlert(for: error)
-                }
-                return
-            }
-            self.heightLabel.text = String(sample.quantity.doubleValue(for: HKUnit(from: "ft")))
-            self.heightLabel.text?.append(" ft")
-        }
-        healthDetails.readSampleHealthData(for: sodiumSampleType){
-            (sample, error) in
-            guard let sample = sample else {
-                if let error = error {
-                    self.dislayAlert(for: error)
-                }
-                return
-            }
-            self.sodiumLabel.text = String(sample.quantity.doubleValue(for: HKUnit(from: "mg")))
-            self.sodiumLabel.text?.append(" mg/dL")
-        }
-    }
     private func dislayAlert(for error : Error){
         let alert = UIAlertController(title: nil, message: error.localizedDescription, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "O.K.", style: .default, handler: nil))
