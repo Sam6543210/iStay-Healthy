c6d31ee456555f6087fd2a413e0ad49eb8bbdc6b
Sam6543210 <sam.ple6543210@gmail.com>
2021-12-23 23:39:26 +0100

    Changes in product details controller

--- iStay Healthy/Product Details ControllerViewController.swift
+++ iStay Healthy/Product Details ControllerViewController.swift
@@ -13,6 +13,18 @@ let healthKitStore: HKHealthStore = HKHealthStore()
 class Product_Details_ControllerViewController: UIViewController {
 
     @IBOutlet weak var passedProductImge: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+    @IBOutlet weak var poductDescriptionLabel: UILabel!
+    @IBOutlet weak var productEnergyLabel: UILabel!
+    @IBOutlet weak var productFatLabel: UILabel!
+    @IBOutlet weak var productSodiumLabel: UILabel!
+    @IBOutlet weak var productSugarLabel: UILabel!
+    @IBOutlet weak var ageGrpLabel: UILabel!
+    
+    @IBOutlet weak var addToCartButton: UIButton!
+    @IBAction func addToCartAction(_ sender: Any) {
+    }
     
     var selectedProduct: Product? = nil
     let healthDetails = HealthDetails_ViewController.init()
@@ -26,6 +38,17 @@ class Product_Details_ControllerViewController: UIViewController {
     func setUpProductDetail()
     {
         self.passedProductImge.image = UIImage(named:(selectedProduct?.productImage)!)
+        self.productNameLabel.text = selectedProduct?.productName
+        self.productNameLabel.frame = CGRect(x: 20, y: 350, width: self.productNameLabel.intrinsicContentSize.width, height: self.productNameLabel.intrinsicContentSize.height)
+        self.productPriceLabel.text?.append("\(String(selectedProduct!.productPrice))")
+        self.ageGrpLabel.text?.append("\(String(selectedProduct!.startingAge))-\(String(selectedProduct!.endingAge)))")
+        self.ageGrpLabel.frame = CGRect(x: 200, y: 380, width: self.ageGrpLabel.intrinsicContentSize.width, height: self.ageGrpLabel.intrinsicContentSize.height)
+        self.poductDescriptionLabel.text?.append("\(String((selectedProduct?.productDescription)!))")
+        self.poductDescriptionLabel.frame = CGRect(x: 20, y: 410, width: self.poductDescriptionLabel.intrinsicContentSize.width, height: self.poductDescriptionLabel.intrinsicContentSize.height)
+        self.productEnergyLabel.text = "\(String(describing: (selectedProduct?.energy)!))"
+        self.productFatLabel.text = "\(String(describing: (selectedProduct?.fatContent)!))"
+        self.productSugarLabel.text = "\(String(describing: (selectedProduct?.sugarContent)!))"
+        self.productSodiumLabel.text = "\(String(describing: (selectedProduct?.sodium)!))"
         
     }
     
